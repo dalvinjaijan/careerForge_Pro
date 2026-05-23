4bafff752b61b09e0b862f22cb6a85762c028b0b
dalvinjaijan <dalvinjaijan2002@gmail.com>
2026-05-24 00:00:03 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import app from "./src/app.js"
+import dotenv from "dotenv"
+dotenv.config()
 
 
 const port=process.env.PORT ||3001
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from "express"
-
+import  {connectDb}  from "./config/mongoDb.js"
 const app = express()
+
+connectDb()
 
 export default app
