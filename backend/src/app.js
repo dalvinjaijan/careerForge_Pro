bb43471f6ade3485e5a7e88d65d0f7387d90a734
dalvinjaijan <dalvinjaijan2002@gmail.com>
2026-05-24 00:05:42 +0530

    frontend initiated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 import express from "express"
 import  {connectDb}  from "./config/mongoDb.js"
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app = express()
+
 
 connectDb()
 
--- backend/src/config/mongoDb.js
+++ backend/src/config/mongoDb.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
 
+
 export const connectDb = async () => {
     try {
    await mongoose.connect(process.env.MONGO_URI)
