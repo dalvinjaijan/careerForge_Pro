5b2e7132cd121523d4e8dda58380c7c25ab220cf
dalvinjaijan <dalvinjaijan2002@gmail.com>
2026-05-25 23:43:32 +0530

    fontend backend connection done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,20 @@
 import express from "express"
 import  {connectDb}  from "./config/mongoDb.js"
 import dotenv from 'dotenv'
+import cors from "cors";
+import cookieParser from "cookie-parser"
+
+
 dotenv.config()
 
 const app = express()
 
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+app.use(express.json())
+app.use(cookieParser())
 connectDb()
 
 export default app
